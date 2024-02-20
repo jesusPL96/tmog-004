0949e8d5d9b554c7df1e0d4f1f533564f9825b6f
Jesús Peiró López <jpeirolo@mercadona.es>
2024-02-20 16:20:06 +0100

    feat: Solution with tests using linked list

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,50 @@
 package org.tfoc;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public ListNode mergeTwoLists(
+    public static ListNode mergeTwoLists(
             ListNode list1,
             ListNode list2) {
 
-        return null;
+        if(list1 == null && list2 != null){
+            return list2;
+        } else if(list2 == null && list1 != null){
+            return list1;
+        } else if(list1 == null){
+            return null;
+        }
+
+        ListNode listNode1 = list1;
+        ListNode listNode2 = list2;
+        LinkedList<Integer> sortingList = new LinkedList<>();
+
+        while (listNode1 != null) {
+            sortingList.add(listNode1.val);
+            listNode1 = listNode1.next;
+        }
+
+        while (listNode2 != null) {
+            sortingList.add(listNode2.val);
+            listNode2 = listNode2.next;
+        }
+
+        Collections.sort(sortingList);
+
+        ListNode testing = new ListNode(sortingList.get(0));
+        ListNode current = testing;
+
+        for(int i = 1; i<sortingList.size(); i++){
+            current.next = new ListNode(sortingList.get(i));
+            current = current.next;
+        }
+
+        return testing;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -3,13 +3,89 @@ package org.tfoc;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 @Slf4j
 class SolutionTest {
 
     @Test
-    void testSolution() {
+    void testSuccessfulSolution() {
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(5);
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(6);
+
+        ListNode expected = new ListNode(1);
+        expected.next = new ListNode(2);
+        expected.next.next = new ListNode(3);
+        expected.next.next.next = new ListNode(4);
+        expected.next.next.next.next = new ListNode(5);
+        expected.next.next.next.next.next = new ListNode(6);
+
+        var result = Solution.mergeTwoLists(l1, l2);
+
+        assertListNodes(expected, result);
+
+    }
+
+    @Test
+    void testOneEmptyListNodeSolution(){
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(6);
+
+        ListNode expected = new ListNode(2);
+        expected.next = new ListNode(4);
+        expected.next.next = new ListNode(6);
+
+        var result = Solution.mergeTwoLists(null, l2);
+
+        assertListNodes(expected, result);
+    }
+
+    @Test
+    void testOtherEmptyListNodeSolution(){
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(6);
+
+        ListNode expected = new ListNode(2);
+        expected.next = new ListNode(4);
+        expected.next.next = new ListNode(6);
+
+        var result = Solution.mergeTwoLists(l2, null);
+
+        assertListNodes(expected, result);
+    }
+
+    @Test
+    void testBothEmptyListNodeSolution(){
+
+        ListNode l1 = null;
+
+        ListNode l2 = null;
+
+        var result = Solution.mergeTwoLists(l1, l2);
+
+        assertNull(result);
+    }
+
+    private void assertListNodes(ListNode expected, ListNode actual) {
+
+        while (expected != null && actual != null) {
+            assertEquals(expected.val, actual.val);
+            expected = expected.next;
+            actual = actual.next;
+        }
+
+        assertEquals(expected, actual);
     }
 
 }
